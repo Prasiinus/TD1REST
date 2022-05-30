7be8620067dce986b8fe1ac57e5b1b6a801cd739
Prasiinus <valentind9@hotmail.fr>
2022-05-30 17:33:26 +0200

    Ajout des query

--- TD1_REST/src/main/java/com/inti/TD1_REST/repository/EcoleRepository.java
+++ TD1_REST/src/main/java/com/inti/TD1_REST/repository/EcoleRepository.java
@@ -1,12 +1,19 @@
 package com.inti.TD1_REST.repository;
 
 import com.inti.TD1_REST.model.Ecole;
-
+import com.inti.TD1_REST.model.Etudiant;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EcoleRepository extends JpaRepository<Ecole, Integer> {
 
+	@Query("FROM Etudiant etu, Ecole eco WHERE etu.ecole_id=eco.id	AND eco.nom=NDA")
+	Etudiant findEtudiantInSchool();
+	
+	@Query("From etudiant etu, ecole eco WHERE etu.ecole_id = eco.id AND ville=RBX")
+	Etudiant findEtudiantFromRBX();
 }
