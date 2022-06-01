e6ff14a83aa16b0c791c6941b1ff2691e3cdaa01
Prasiinus <valentind9@hotmail.fr>
2022-06-01 11:31:02 +0200

    comm

--- TD1_REST/src/test/java/com/inti/TD1_REST/controller/EtudiantControllerTest.java
+++ TD1_REST/src/test/java/com/inti/TD1_REST/controller/EtudiantControllerTest.java
@@ -61,7 +61,7 @@ public class EtudiantControllerTest {
 			mockMvc.perform(get("/etudiantsInLyon"))
 				.andExpect(status().isOk());
 		} catch (Exception e) {
-			e.printStackTrace();
+			e.printStackTrace();   
 		}	
 	}
 
